3dcd36ccbd312e492569244b6756a3914af319b7
Ed Skoviak <eskoviak@gmail.com>
2020-09-07 12:54:12 -0500

    from surface

--- covid/covid1.R
+++ covid/covid1.R
@@ -1,4 +1,4 @@
 
 
-confirmedDF = read.csv("d:/source/repos/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_us.csv", check.names = FALSE)
+confirmedDF = read.csv("d:/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_us.csv", check.names = FALSE)
 deathDF = read.csv("d:/source/repos/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_us.csv")
