6f1c9cdb794cb1be08604e3b57112759bab5ea95
Ed <eskoviak@eskoviak.com>
2020-09-09 20:07:55 -0500

    EOD

--- covid/covid1.R
+++ covid/covid1.R
@@ -1,10 +1,13 @@
 
+# Load the data set and tidy up
+library(readr)
+confirmedDF = read_csv("d:/source/repos/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_us.csv")
 
-confirmedDF = read.csv("d:/source/repos/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_us.csv", check.names = FALSE)
+
+
 datesDT = as.Date(names(confirmedDF[,-(1:11)]), "%m/%d/%y")
-confirmed = mapply(sum, confirmedDF[(confirmedDF$Province_State == 'Minnesota'), -(1:11)])
-confirmedV <- c()
-append(confirmedV, confirmed)
+confirmed = mapply(sum, confirmedDF[(confirmedDF$Province_State == 'Minnesota'), -(1:11)], USE.NAMES = FALSE)
+names(confirmed) <- datesDT
 
 library(ggplot2)
 
