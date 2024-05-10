3da5d7542e1fe153bfc498cabb9a3303cf7b5f68
Ed Skoviak <eskoviak@gmail.com>
2024-05-10 18:59:05 -0500

    eod mac

--- loop 1.R
+++ loop 1.R
@@ -1,6 +1,11 @@
+type_list <- c("CashandCarry", "PayrollDeduction", "Subsidy", "SubsidyandPayrollDeduction", "SubsidywithPcard")
 test_data <- structure(list(p=as.numeric(),
-                            type=character()),
+                            type=character(),
+                            s=as.numeric(),
+                            s_type=character,
+                            s_max=as.numeric()),
                        class="data.frame")
+
 for (i in 1:nrow(tax_rules)) {
   p <- as.numeric(tax_rules[i,'paid sub amt']+tax_rules[i,'paid cons amt']+tax_rules[i,'paid PD amt'])
   if(is.na(p)) {next}
@@ -14,6 +19,13 @@ for (i in 1:nrow(tax_rules)) {
   tacc[4] <- as.numeric(gsub('%','',tax_rules[i,18]))/100
   tacc[is.na(tacc)] <- 0
   tcon <- tacc
-  newrow = data.frame(p=p,type=type)
+  
+
+  if(grep("#", tax_rules[1,4])) { s <- 0; s_type <- "NA"; s_max <- 0}
+  else {
+    if(grep("[$]",tax_rules[i,4])) { s <- as.numeric(gsub('$','',tax_rules[i,4])); s_type="dollar"; s_max=0 }
+    if(grep("[%]",tax_rules[i,4])) {}
+  }
+  newrow = data.frame(p=p,type=type, s=s, s_type=s_type, s_max=s_max)
   test_data <- rbind(test_data, newrow)
 }
--- reader.R
+++ reader.R
@@ -1,3 +1,4 @@
 library(readr)
 tax_rules <- read_csv("~/repos/r-playgroud/tax_rules_with_examples_for_EVA.csv")
+
 #View(tax_rules)
